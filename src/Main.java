652f3e7663bea5b92b4ff011d3ccb80490d546aa
Zephoenixen <stormriberengelund@gmail.com>
2026-09-18 18:46:07 +0200

    Populate books and members - Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,30 @@ void main(String[] args) {
     Library library = new Library();
     ConsoleUI consoleUI = new ConsoleUI(library);
 
+    Book book1 = new Book("Allan Cooper", "The easiest way to bad code", "805", 1);
+    Book book2 = new Book("Sarah Nova", "Bear Hits You", "2016", 2);
+    Book book3 = new Book("Rajeh Aiter", "Your Friends hate me", "52106", 3);
+
+    Member memb1 = new Member("James", 201);
+    Member memb2 = new Member("Luna", 202);
+    Member memb3 = new Member("Ashley", 203);
+
+    library.addBook(book1);
+    library.addBook(book2);
+    library.addBook(book3);
+
+    library.addMember(memb1);
+    library.addMember(memb2);
+    library.addMember(memb3);
+
     consoleUI.run();
 
 
-    {/*
-        Book book1 = new Book("Allan Cooper", "MongoMethods", "805", 1);
-        Book book2 = new Book("Sarah Nova", "Bear Hits You", "2016", 2);
-        Book book3 = new Book("Ragebaiter", "Fuck all y'all", "52106", 3);
-        Member memb1 = new Member("James", 87);
-        Member memb2 = new Member("Luna", 01613);
-
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-        library.addMember(memb1);
-        library.addMember(memb2);
+    /*
         IO.println(library.loanBook(1, 87));
         IO.println(library.loanBook(2, 01613));
         library.printLoans();
         library.returnBook(2);
-        IO.println(library.loanBook(2, 87));
-
-        IO.println(library.findLoansByMemberID(87));
-    */}
+    */
 
 }
